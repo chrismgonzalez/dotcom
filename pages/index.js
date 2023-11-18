dbf1f5c161899327dd546a99921f14f6c511feba
Chris Gonzalez <cmgonza89@gmail.com>
2023-11-18 03:52:28 +0000

    posts are working with links

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Link from 'next/link';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
+import homeStyles from '../styles/Home.module.css';
+import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Intro here]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        <p className={homeStyles.description}> Cloud Architect | Creator | Educator </p>
+        <p className={homeStyles.description}>
+          Welcome! Enjoy your time as you browse my blogs and tutorials.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Articles</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>  
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
---
+++ posts/reinvent-2023-recap.md
@@ -0,0 +1,6 @@
+---
+title: 'Re:Invent 2023 Recap'
+date: '2023-12-01'
+---
+
+This will be a post about Re:Invent 2023
